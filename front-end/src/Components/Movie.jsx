1eb08d99168df060fb7edd3ad5a6b23292fd3d9b
Nicole Slater <nicoleslater@pursuit.org>
2023-11-09 23:42:26 -0500

    Updating Movie.jsx

--- front-end/src/Components/Movie.jsx
+++ front-end/src/Components/Movie.jsx
@@ -1,5 +1,25 @@
-function Movie({}){
+import {Link} from "react-router-dom";
 
+function Movie({movie}){
+return (
+    <tr>
+        <td> 
+            {movie.is_favorite ? (
+                <span>🍿</span>
+            ) : (
+                <span>&nbsp; &nbsp; &nbsp;</span>
+            )}
+        </td>
+        <tdc style={{cursor: "alias"}}>
+        <a href={movie.released} target="_blank" rel="noreferrer">
+            {movie.title}
+        </a>
+            </td>
+            <td>
+                <Link to={`/movies/${movie.id}`}>🎥</Link>
+            </td>
+    </tr>
+);
 }
 
 export default Movie;
